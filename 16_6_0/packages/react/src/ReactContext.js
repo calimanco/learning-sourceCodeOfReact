ba57946e0cfbd76d0a62cc66a1c2f05eaeca2bd0
linli <linli@hongrz.com>
2019-11-26 18:25:12 +0800

    更新ReactContext相关

--- 16_6_0/packages/react/src/ReactContext.js
+++ 16_6_0/packages/react/src/ReactContext.js
@@ -45,17 +45,21 @@ export function createContext<T>(
     // Fabric (secondary); React DOM (primary) and React ART (secondary).
     // Secondary renderers store their context values on separate fields.
     // 翻译：作为解决多个并发渲染器的解决方法，我们将一些渲染器归类为主要渲染器，将其他渲染器归类为次要渲染器。
-    //      我们只希望最多有两个并发渲染器：React Native（主要）和Fabric（次要）；React DOM（主要）和React ART（次要）。
+    //      我们只希望最多有两个并发渲染器：React Native（主要）和Fabric（次要）；
+    //      React DOM（主要）和React ART（次要）。
     //      辅助渲染器将其上下文值存储在单独的字段中。
     // 这里提供两个字段（具体使用跟平台有关）用来保存当前值，默认为传入函数的defaultValue。
     _currentValue: defaultValue,
     _currentValue2: defaultValue,
     // These are circular
-    // 翻译：
+    // 翻译：这些是循环的。
+    // 下面两个暴露的组件里Provider的_context属性就是指向context，Consumer本身就是指向context，
+    // 所以是循环引用。
     Provider: (null: any),
     Consumer: (null: any),
   };
 
+  // 给暴露的Provider增加属性
   context.Provider = {
     $$typeof: REACT_PROVIDER_TYPE,
     _context: context,
@@ -68,12 +72,16 @@ export function createContext<T>(
     // A separate object, but proxies back to the original context object for
     // backwards compatibility. It has a different $$typeof, so we can properly
     // warn for the incorrect usage of Context as a Consumer.
+    // 翻译：一个单独的对象，但是代理返回到原始上下文对象，以实现向后兼容。它有一个不同$$typeof，
+    //      因此我们可以适当地警告将Context作为Consumer使用不正确。
     const Consumer = {
       $$typeof: REACT_CONTEXT_TYPE,
       _context: context,
       _calculateChangedBits: context._calculateChangedBits,
     };
     // $FlowFixMe: Flow complains about not setting a value, which is intentional here
+    // Flow的特殊注释，后面有报错才会提示。
+    // 翻译：Flow提示没有设置值，这是故意的。
     Object.defineProperties(Consumer, {
       Provider: {
         get() {
@@ -122,8 +130,11 @@ export function createContext<T>(
       },
     });
     // $FlowFixMe: Flow complains about missing properties because it doesn't understand defineProperty
+    // Flow的特殊注释，后面有报错才会提示。
+    // 翻译：Flow提示缺少属性，因为它不了解defineProperty.
     context.Consumer = Consumer;
   } else {
+    // 给暴露的Consumer直接指向了context本身
     context.Consumer = context;
   }
 
