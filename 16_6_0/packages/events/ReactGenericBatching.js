7117720aabbba3f9078f8aa605519bb940a32283
calimanco <calimanco@qq.com>
2020-06-17 15:39:01 +0800

    update

--- 16_6_0/packages/events/ReactGenericBatching.js
+++ 16_6_0/packages/events/ReactGenericBatching.js
@@ -15,6 +15,10 @@ import {
 // libraries need to call batchedUpdates. Eventually, this API will go away when
 // everything is batched by default. We'll then have a similar API to opt-out of
 // scheduled work and instead do synchronous work.
+// 翻译：当我们没有对渲染器的参考时，用作调用batchedUpdates的方式。
+//      例如，当我们调度事件或第三方库需要调用batchedUpdates时。
+//      最终，当默认情况下所有批次都被批处理时，此API将消失。
+//      然后，我们将有一个类似的API来选择退出调度工作，或进行同步工作。
 
 // Defaults
 let _batchedUpdatesImpl = function(fn, bookkeeping) {
@@ -30,6 +34,7 @@ export function batchedUpdates(fn, bookkeeping) {
   if (isBatching) {
     // If we are currently inside another batch, we need to wait until it
     // fully completes before restoring state.
+    // 翻译：如果我们当前在另一个批次中，则需要等到它完全完成后再恢复状态。
     return fn(bookkeeping);
   }
   isBatching = true;
@@ -40,12 +45,17 @@ export function batchedUpdates(fn, bookkeeping) {
     // when using controlled components within layers:
     // https://github.com/facebook/react/issues/1698
     // Then we restore state of any controlled component.
+    // 翻译：在这里，我们等待所有更新均已完成，这对于在层中使用受控组件时非常重要：
+    //      https://github.com/facebook/react/issues/1698
+    //      然后，我们恢复任何受控组件的状态。
     isBatching = false;
     const controlledComponentsHavePendingUpdates = needsStateRestore();
     if (controlledComponentsHavePendingUpdates) {
       // If a controlled event was fired, we may need to restore the state of
       // the DOM node back to the controlled value. This is necessary when React
       // bails out of the update without touching the DOM.
+      // 翻译：如果触发了受控事件，则可能需要将DOM节点的状态恢复回受控值。
+      //      当React退出更新而不接触DOM时，这是必需的。
       _flushInteractiveUpdatesImpl();
       restoreStateIfNeeded();
     }
--- 16_6_0/packages/react-dom/src/events/ReactDOMEventListener.js
+++ 16_6_0/packages/react-dom/src/events/ReactDOMEventListener.js
@@ -31,6 +31,8 @@ const callbackBookkeepingPool = [];
  * Find the deepest React component completely containing the root of the
  * passed-in instance (for use when entire React trees are nested within each
  * other). If React trees are not nested, returns null.
+ * 翻译：查找完全包含传入实例根的最深的React组件（用于将整个React树相互嵌套时使用）。
+ *      如果React树没有嵌套，则返回null。
  */
 function findRootContainerNode(inst) {
   // TODO: It may be a good idea to cache this to prevent unnecessary DOM
@@ -41,12 +43,15 @@ function findRootContainerNode(inst) {
   }
   if (inst.tag !== HostRoot) {
     // This can happen if we're in a detached tree.
+    // 翻译：如果我们在分离的树中，可能会发生这种情况。
     return null;
   }
+  // RootFiber的stateNode是FiberRoot，FiberRoot的containerInfo是React树挂载的DOM节点。
   return inst.stateNode.containerInfo;
 }
 
 // Used to store ancestor hierarchy in top level callback
+// 翻译：用于将祖先层次结构存储在顶级回调中。
 function getTopLevelCallbackBookKeeping(
   topLevelType,
   nativeEvent,
@@ -82,24 +87,35 @@ function releaseTopLevelCallbackBookKeeping(instance) {
   }
 }
 
+/**
+ * 处理事件
+ * @param bookKeeping 保存事件信息的对象
+ */
 function handleTopLevel(bookKeeping) {
+  // targetDOM节点的Fiber对象。
   let targetInst = bookKeeping.targetInst;
 
   // Loop through the hierarchy, in case there's any nested components.
   // It's important that we build the array of ancestors before calling any
   // event handlers, because event handlers can modify the DOM, leading to
   // inconsistencies with ReactMount's node cache. See #1105.
+  // 翻译：遍历层次结构，以防存在任何嵌套的组件。
+  //      在调用任何事件处理程序之前，先构建祖先数组非常重要，因为事件处理程序可以修改DOM，
+  //      从而导致与ReactMount的节点缓存不一致。 请参阅＃1105。
   let ancestor = targetInst;
   do {
     if (!ancestor) {
       bookKeeping.ancestors.push(ancestor);
       break;
     }
+    // 向上找RootFiber节点，找到则返回Container的DOM节点，否则返回null。
     const root = findRootContainerNode(ancestor);
     if (!root) {
       break;
     }
     bookKeeping.ancestors.push(ancestor);
+    // 继续向上找Fiber节点，正常来说Container的DOM节点已经没有Fiber对象了。
+    // 但不排除React实例嵌套React实例进行渲染的情况。
     ancestor = getClosestInstanceFromNode(root);
   } while (ancestor);
 
@@ -127,6 +143,7 @@ export function isEnabled() {
 
 /**
  * Traps top-level events by using event bubbling.
+ * 翻译：通过使用事件冒泡来捕获顶级事件。
  *
  * @param {number} topLevelType Number from `TopLevelEventTypes`.
  * @param {object} element Element on which to attach listener.
@@ -141,6 +158,7 @@ export function trapBubbledEvent(
   if (!element) {
     return null;
   }
+  // 这里是判断是不是交互式事件，两者的差别只是过期时间不同，交互式事件需要更即时的更新。
   const dispatch = isInteractiveTopLevelEventType(topLevelType)
     ? dispatchInteractiveEvent
     : dispatchEvent;
@@ -149,6 +167,7 @@ export function trapBubbledEvent(
     element,
     getRawEventName(topLevelType),
     // Check if interactive and wrap in interactiveUpdates
+    // 翻译：检查是否交互式并包装交互式更新。
     dispatch.bind(null, topLevelType),
   );
 }
@@ -182,9 +201,15 @@ export function trapCapturedEvent(
 }
 
 function dispatchInteractiveEvent(topLevelType, nativeEvent) {
+  // 下面这个函数来自packages/react-reconciler/src/ReactFiberScheduler.js
   interactiveUpdates(dispatchEvent, topLevelType, nativeEvent);
 }
 
+/**
+ * 派发事件。
+ * @param topLevelType 顶级事件类型
+ * @param nativeEvent 事件触发后浏览器传入的原生对象
+ */
 export function dispatchEvent(
   topLevelType: DOMTopLevelEventType,
   nativeEvent: AnyNativeEvent,
@@ -193,20 +218,29 @@ export function dispatchEvent(
     return;
   }
 
+  // 兼容各种环境获取target的方法（就是触发事件的DOM节点）。
   const nativeEventTarget = getEventTarget(nativeEvent);
+  // 获取Fiber对象，如果当前的节点没有会向上找到一个有的。
   let targetInst = getClosestInstanceFromNode(nativeEventTarget);
   if (
     targetInst !== null &&
     typeof targetInst.tag === 'number' &&
+    // 判断当前DOM是否已经挂载。
     !isFiberMounted(targetInst)
   ) {
+    // 这个情况说明节点还未被挂载，即使已经有Fiber也要认为没有。
     // If we get an event (ex: img onload) before committing that
     // component's mount, ignore it for now (that is, treat it as if it was an
     // event on a non-React tree). We might also consider queueing events and
     // dispatching them after the mount.
+    // 翻译：如果我们在提交组件挂载之前接到一个事件（比如：img的onload事件），
+    //      请暂时将其忽略（即，将其视为非响应树上的事件）。
+    //      我们也可以考虑对事件进行排队，然后在挂载后分派它们。
     targetInst = null;
   }
 
+  // 创建一个bookKeeping对象保存现有的信息，这个对象会被保存到对象池重复使用。
+  // 形如{topLevelType、nativeEvent、targetInst、ancestors}
   const bookKeeping = getTopLevelCallbackBookKeeping(
     topLevelType,
     nativeEvent,
@@ -216,6 +250,8 @@ export function dispatchEvent(
   try {
     // Event queue being processed in the same cycle allows
     // `preventDefault`.
+    // 翻译：在同一周期中处理的事件队列允许使用“ preventDefault”。
+    // 这个函数也是
     batchedUpdates(handleTopLevel, bookKeeping);
   } finally {
     releaseTopLevelCallbackBookKeeping(bookKeeping);
